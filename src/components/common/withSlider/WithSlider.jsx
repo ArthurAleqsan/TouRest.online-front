253f185ed1a5c229aba3d7c1894fdf8f59e97352
LTonyan <liana.tonian@gmail.com>
2021-05-31 19:08:49 +0400

    add new logo

--- src/components/common/withSlider/WithSlider.jsx
+++ src/components/common/withSlider/WithSlider.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import { WithTitle } from '../withTitle/WithTitle';
 import Tabs from '../Tabs';
 import Slider from '../Slider';
 import ToursPage from '../../../containers/pages/ToursPage';
-import Taxi from '../../../containers/pages/Taxi';
 import CategoryContainer from '../../../containers/pages/category/CategoryContainer';
 import Tours from '../../../containers/pages/Tours';
 import TourBookingPage from '../../../containers/pages/TourBookingPage';
@@ -17,12 +15,12 @@ const WithSlider = () => {
             <Slider />
             {!isTaxiPage && <Tabs />}
             <Switch>
-                <Route path='/:location/:lng/categories' component={() => (<WithTitle title='Choose tour category'><CategoryContainer /></WithTitle>)} />
-                <Route path='/:location/:lng/tours/:id' component={() => (<WithTitle title='Choose best tours'><Tours /></WithTitle>)} />
-                <Route path='/:location/:lng/tours-today' component={() => (<WithTitle title='Todays avaiable tours'><ToursPage headerName={'Tours Today'} fromToursToday={true} /></WithTitle>)} />
-                <Route path='/:location/:lng/vip-tours' component={() => (<WithTitle title='VIP Tours'><ToursPage headerName='VIP Tours' fromToursToday={false} /></WithTitle>)} />
-                <Route path='/:location/:lng/tour-booking' component={() => (<WithTitle title='Book Tour'><TourBookingPage /></WithTitle>)} />
-                <Route path='/:location/:lng' component={() => (<WithTitle><ToursPage headerName='Most Popular' fromToursToday={false} /></WithTitle>)} />
+                <Route path='/:location/:lng/categories' component={() => (<CategoryContainer />)} />
+                <Route path='/:location/:lng/tours/:id' component={() => (<Tours />)} />
+                <Route path='/:location/:lng/tours-today' component={() => (<ToursPage headerName={'Tours Today'} fromToursToday={true} />)} />
+                <Route path='/:location/:lng/vip-tours' component={() => (<ToursPage headerName='VIP Tours' fromToursToday={false} />)} />
+                <Route path='/:location/:lng/tour-booking' component={() => (<TourBookingPage />)} />
+                <Route path='/:location/:lng' component={() => (<ToursPage headerName='Most Popular' fromToursToday={false} />)} />
             </Switch>
         </div>
     )
