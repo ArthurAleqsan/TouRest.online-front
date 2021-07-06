33b728fc663eb5a0e66c900d2d9ee93cfec8cf3d
LTonyan <liana.tonian@gmail.com>
2021-07-06 18:04:07 +0400

    chnages in fb sharing

--- index.js
+++ index.js
@@ -56,14 +56,14 @@ app.use('/public', express.static('public'));
 app.use('/locales', express.static('locales'));
 app.use('/assets', express.static('assets'));
 
-app.get(‘/’,  (request, response) => {
-    const filePath = path.resolve(__dirname, ‘./public’, ‘index.html’);
-    fs.readFile(filePath, ‘utf8’, function (err,data) {
+app.get('/', async  (request, response) => {
+    const filePath = path.resolve(__dirname, './public', 'index.html');
+    fs.readFile(filePath, 'utf8', function (err,data) {
         if (err) {
             return console.log(err);
         }
-        data = data.replace(/TouRest Online/g, ‘TouRest Online’);
-        data = data.replace(/\$OG_DESCRIPTION/g, ‘Everyone deserves to feel the delight of traveling, so, our founding team decided to be one of the firsts to give that chance. TouRest was founded in 2019 by some programmers who are devoted to their job.‘);
+        data = data.replace(/TouRest Online/g, 'TouRest Online');
+        data = data.replace(/\$OG_DESCRIPTION/g, 'Everyone deserves to feel the delight of traveling, so, our founding team decided to be one of the firsts to give that chance. TouRest was founded in 2019 by some programmers who are devoted to their job.');
         result = data.replace(/\$OG_IMAGE/g, ‘’);
         response.send(result);
     });
